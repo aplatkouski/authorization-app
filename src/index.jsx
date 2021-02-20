7230956843276251b8b2a075a2230be1b08535cc
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-21 00:02:11 +0300

    refactor: move app.scss and reset.scss from src/style/ to src/
    - rename app.scss to index.scss and move 'import' statement from App.jsx to index.jsx;
    
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 
 import store from 'States/root-store';
 import App from './App';
+import './index.scss';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(

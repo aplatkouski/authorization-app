b26c39a743c6780beb7ce3531bc733a1c924e55f
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-21 00:41:05 +0300

    feat: add state.user.isAuth as props to main-view component
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/MainView/MainViewContainer.js
+++ src/components/MainView/MainViewContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import MainView from './MainView';
 
 const mapStateToProps = (state) => ({
+  isAuth: state.user.isAuth,
   isLoading: state.database.isLoading,
 });
 

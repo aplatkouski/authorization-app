1372ad9454392866affcdceb368188f8d6939da0
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-21 23:42:14 +0300

    feat: handle block and unblock users
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/UserTable/UserTable.jsx
+++ src/components/UserTable/UserTable.jsx
@@ -1,79 +1,86 @@
 import PropTypes from 'prop-types';
+import { useRef } from 'react';
 import * as React from 'react';
 import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import { useDispatch } from 'react-redux';
+import { userActions } from 'States/user';
 
-class UserTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.columns = [
-      {
-        dataField: 'id',
-        text: 'ID',
-      },
-      {
-        dataField: 'username',
-        text: 'Username',
-      },
-      {
-        dataField: 'email',
-        text: 'Email',
-      },
-      {
-        dataField: 'registrationDate',
-        text: 'Registration Date',
-      },
-      {
-        dataField: 'lastLoginDate',
-        text: 'Last Login Date',
-      },
-      {
-        dataField: 'isActive',
-        text: 'Status',
-      },
-    ];
+const UserTable = ({ users }) => {
+  const dispatch = useDispatch();
+  const columns = [
+    {
+      dataField: 'id',
+      text: 'ID',
+    },
+    {
+      dataField: 'username',
+      text: 'Username',
+    },
+    {
+      dataField: 'email',
+      text: 'Email',
+    },
+    {
+      dataField: 'registrationDate',
+      text: 'Registration Date',
+    },
+    {
+      dataField: 'lastLoginDate',
+      text: 'Last Login Date',
+    },
+    {
+      dataField: 'isActive',
+      text: 'Status',
+    },
+  ];
 
-    this.handleBlockUsers = this.handleBlockUsers.bind(this);
-    this.renderCaptionElement = this.renderCaptionElement.bind(this);
-  }
+  const node = useRef(null);
 
-  renderCaptionElement = () => {
+  const handleBlockUsers = () => {
+    node.current.selectionContext.selected.forEach((uid) => {
+      dispatch(userActions.blockUser(users.find((user) => user.uid === uid)));
+    });
+  };
+
+  const handleUnblockUsers = () => {
+    node.current.selectionContext.selected.forEach((uid) => {
+      dispatch(userActions.unblockUser(users.find((user) => user.uid === uid)));
+    });
+  };
+
+  const renderCaptionElement = () => {
     return (
       <ButtonToolbar aria-label="Toolbar with button groups">
         <ButtonGroup className="mb-2" size="lg">
-          <Button onClick={this.handleBlockUsers} variant="outline-warning">
+          <Button onClick={handleBlockUsers} variant="outline-warning">
             Block
           </Button>
-          <Button variant="outline-success">Unblock</Button>
+          <Button onClick={handleUnblockUsers} variant="outline-success">
+            Unblock
+          </Button>
           <Button variant="outline-danger">Delete</Button>
         </ButtonGroup>
       </ButtonToolbar>
     );
   };
 
-  handleBlockUsers = () => {};
-
-  render() {
-    const { users } = this.props;
-    return (
-      <BootstrapTable
-        ref={(n) => {
-          this.node = n;
-        }}
-        caption={this.renderCaptionElement()}
-        columns={this.columns}
-        data={users}
-        hover
-        keyField="uid"
-        noDataIndication="Table is Empty"
-        pagination={paginationFactory()}
-        selectRow={{ mode: 'checkbox', clickToSelect: true }}
-        striped
-      />
-    );
-  }
-}
+  return (
+    <BootstrapTable
+      ref={node}
+      caption={renderCaptionElement()}
+      columns={columns}
+      data={users}
+      hover
+      keyField="uid"
+      noDataIndication="Table is Empty"
+      pagination={paginationFactory()}
+      selectRow={{ mode: 'checkbox', clickToSelect: true }}
+      striped
+    />
+  );
+};
 
 UserTable.propTypes = {
   users: PropTypes.arrayOf(

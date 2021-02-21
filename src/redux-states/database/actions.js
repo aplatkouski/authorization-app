db9ac0c8e6a741cb3374251e609292690da772ca
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-21 14:17:04 +0300

    feat: add 'put user' and 'fetch users' actions to 'database' redux-state
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/redux-states/database/actions.js
+++ src/redux-states/database/actions.js
@@ -1,3 +1,4 @@
+import { database } from 'Utils/firebase';
 import * as t from './action-types';
 
 export const showLoader = () => ({
@@ -7,3 +8,21 @@ export const showLoader = () => ({
 export const hideLoader = () => ({
   type: t.HIDE_LOADER,
 });
+
+export const putUserData = async (user) => {
+  const { uid, ...rest } = user;
+  const userRef = database.ref(`users/${uid}`);
+  await userRef.set({ ...rest });
+  return Promise.resolve(user);
+};
+
+export const fetchUsers = async () => {
+  const userListRef = database.ref(`users/`);
+  const users = [];
+  await userListRef.once('value', (usersSnapshot) => {
+    usersSnapshot.forEach((userSnapshot) => {
+      users.push({ ...userSnapshot.val(), uid: userSnapshot.key });
+    });
+  });
+  return Promise.resolve(users);
+};

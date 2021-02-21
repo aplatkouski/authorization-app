903417c96783b3df02dca387928ba4e7c5063f1b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-21 13:53:42 +0300

    feat: add thunk and logger redux-middleware
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/redux-states/root-store.js
+++ src/redux-states/root-store.js
@@ -1,8 +1,15 @@
 import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composedEnhancer = composeWithDevTools({ trace: true, traceLimit: 25 });
+
+const store = createStore(
+  rootReducer,
+  composedEnhancer(applyMiddleware(loggerMiddleware, thunkMiddleware))
+);
 
 export default store;
